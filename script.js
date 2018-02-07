f8c027b8c1b1002aec28afd4bd8bbee24aaf2c5b
KntKonata <manu.foureur@gmail.com>
2018-02-07 16:18:12 +0100

    Few changes in script.js

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-var CustomRandom = function(nseed) {    
-  
-  var seed,    
-    constant = Math.pow(2, 13)+1,    
-    prime = 1987,    
-//any prime number, needed for calculations, 1987 is my favorite:)  
-    maximum = 1000;    
-//maximum number needed for calculation the float precision of the numbers (10^n where n is number of digits after dot)  
-    if (nseed) {    
-      seed = nseed;    
-    }    
-    
-    if (seed == null) {    
-//before you will correct me in this comparison, read Andrea Giammarchi's text about coercion http://goo.gl/N4jCB  
-      
-      seed = (new Date()).getTime();   
-//if there is no seed, use timestamp     
-    }     
-    
+var CustomRandom = function(nseed) {
     return {    
-      next : function(min, max) {    
-        seed *= constant;    
-        seed += prime;    
-           
-      
-        return min && max ? min+seed%maximum/maximum*(max-min) : seed%maximum/maximum;  
-// if 'min' and 'max' are not provided, return random number between 0 & 1  
-      }    
-    }    
-} 
+        next : function(min, max) {
+            return min && max ? min+nseed%1000/1000*(max-min) : nseed%1000/1000;
+        }
+    }
+}
 
 var etudiants = ['Mathias','Chihab','Grégoire','Grégory','Paul','Manu','Dorra','Stéphanie','Nicolas','Frédéric','Maxime','Samir'];
 var test = Math.floor(Date.now()/1000/60/60/24);
